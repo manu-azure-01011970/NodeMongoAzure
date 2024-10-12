0da1b426ec70361fc6f62936ea925e5d9169cf1b
MES <info@manuelestevez.es>
2024-10-12 12:57:26 +0200

    prueba de cambio para funcionamiento de secrets en github actions

--- app/src/app.js
+++ app/src/app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 // Conexión a MongoDB usando la URI del archivo .env
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Conectado a MongoDB'))
+  .then(() => console.log('Conectado al MongoDB'))
   .catch((err) => console.error('Error conectando a MongoDB:', err));
 
 // Middleware
